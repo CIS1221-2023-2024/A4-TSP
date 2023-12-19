82101a8d59654daa24c8b6edc8756aa6c7ed23e7
GiorgioBellia <150366834+GiorgioBellia@users.noreply.github.com>
2023-12-19 15:52:57 +0100

    City class
    Class to represent a city as an object

---
+++ Java code(BlueJ)/City.java
@@ -0,0 +1,29 @@
+public class City{
+    private int x; //X coordinate
+    private int y; //Y coordinate
+    
+    //Constructor
+    City(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    //Getters and Setters
+    void setX(int x){
+        this x = x;
+    }
+    void setY(int y){
+        this y = y;
+    }
+    int getX(){
+        return x;
+    }
+    int getY(){
+        return y;
+    }
+    
+    //Method to calculate the distance between two cities.
+    public static double calculateDistance(City a, City b){
+        return Math.sqrt(Math.pow(b.y-a.y,2)-Math.pow(b.x-a.x,2));
+    }
+}
